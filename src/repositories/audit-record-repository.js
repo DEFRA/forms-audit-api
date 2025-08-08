1eecf84100bd3f76e6d08c8ac723fb0aaa320568
whitewater design <whitewaterdesign>
2025-08-08 13:28:12 +0100

    feat: DF-270 - Add repo method getAuditRecords

--- src/repositories/audit-record-repository.js
+++ src/repositories/audit-record-repository.js
@@ -1,7 +1,32 @@
+import { getErrorMessage } from '~/src/helpers/error-message.js'
 import { createLogger } from '~/src/helpers/logging/logger.js'
 import { AUDIT_RECORDS_COLLECTION_NAME, db } from '~/src/mongo.js'
 
 const logger = createLogger()
+
+const MAX_RECORDS = 100
+
+/**
+ * Gets a filtered list of audit records
+ * @param {Filter<WithId<AuditRecordInput>>} filter
+ * @returns {Promise<WithId<AuditRecordInput>[]>}
+ */
+export async function getAuditRecords(filter) {
+  logger.info('Reading audit records')
+
+  const coll = /** @type {Collection<AuditRecordInput>} */ (
+    db.collection(AUDIT_RECORDS_COLLECTION_NAME)
+  )
+
+  try {
+    const results = await coll.find(filter).limit(MAX_RECORDS).toArray()
+
+    return results
+  } catch (e) {
+    logger.error(`Failed to read audit records - ${getErrorMessage(e)}`)
+    throw e
+  }
+}
 
 /**
  * Creates an audit record from AuditRecordInput
@@ -19,7 +44,7 @@ export async function createAuditRecord(auditRecordInput, session) {
     logger.info(`Inserted ${auditRecordInput.messageId}`)
   } catch (e) {
     logger.error(
-      `Failed to insert ${auditRecordInput.messageId} - ${e.toString()} `
+      `Failed to insert ${auditRecordInput.messageId} - ${getErrorMessage(e)} `
     )
     throw e
   }
@@ -27,7 +52,7 @@ export async function createAuditRecord(auditRecordInput, session) {
 
 /**
  * @import { AuditRecordInput, FormDefinition, Page, ComponentDef, PatchPageFields, List, Engine, ConditionWrapperV2 } from '@defra/forms-model'
- * @import { ClientSession, Collection, FindOptions } from 'mongodb'
+ * @import { ClientSession, Collection, Filter, WithId } from 'mongodb'
  * @import { ObjectSchema } from 'joi'
  * @import { UpdateCallback, RemovePagePredicate } from '~/src/api/forms/repositories/helpers.js'
  */
--- src/repositories/audit-record-repository.test.js
+++ src/repositories/audit-record-repository.test.js
@@ -1,11 +1,19 @@
+import { ObjectId } from 'mongodb'
+
 import {
+  STUB_AUDIT_RECORD_ID,
   buildAuditMetaBase,
+  buildAuditRecordDocument,
+  buildAuditRecordDocumentMeta,
   buildAuditRecordInput,
   buildLiveCreatedFromDraftMessage
 } from '~/src/api/forms/__stubs__/audit.js'
 import { buildMockCollection } from '~/src/api/forms/__stubs__/mongo.js'
 import { db } from '~/src/mongo.js'
-import { createAuditRecord } from '~/src/repositories/audit-record-repository.js'
+import {
+  createAuditRecord,
+  getAuditRecords
+} from '~/src/repositories/audit-record-repository.js'
 
 const mockCollection = buildMockCollection()
 
@@ -51,22 +59,62 @@ jest.mock('~/src/mongo.js', () => {
 })
 
 describe('audit-record-repository', () => {
+  const recordInput = buildAuditMetaBase({
+    recordCreatedAt: new Date('2025-08-08'),
+    messageId: '23b3e93c-5bea-4bcc-ab27-be69ce82a190'
+  })
+  const auditMessage = buildLiveCreatedFromDraftMessage()
+  const auditRecordInput = buildAuditRecordInput(auditMessage, recordInput)
+  const documentId = new ObjectId(STUB_AUDIT_RECORD_ID)
+  const auditDocument = buildAuditRecordDocument(
+    auditMessage,
+    buildAuditRecordDocumentMeta({
+      _id: documentId,
+      ...auditRecordInput
+    })
+  )
+
   beforeEach(() => {
     jest.mocked(db.collection).mockReturnValue(mockCollection)
   })
 
-  describe('createAuditRecord', () => {
-    const recordInput = buildAuditMetaBase({
-      recordCreatedAt: new Date('2025-08-08'),
-      messageId: '23b3e93c-5bea-4bcc-ab27-be69ce82a190'
+  describe('getAuditRecords', () => {
+    it('should get audit records', async () => {
+      const toArrayStub = jest.fn().mockResolvedValueOnce([auditDocument])
+      const limitStub = jest.fn().mockReturnValue({
+        toArray: toArrayStub
+      })
+      mockCollection.find.mockReturnValueOnce({
+        limit: limitStub
+      })
+      const auditRecords = await getAuditRecords({
+        entityId: STUB_AUDIT_RECORD_ID
+      })
+      const [filter] = mockCollection.find.mock.calls[0]
+      expect(filter).toEqual({ entityId: STUB_AUDIT_RECORD_ID })
+      expect(limitStub).toHaveBeenCalledWith(100)
+      expect(auditRecords).toEqual([auditDocument])
     })
-    const auditMessage = buildLiveCreatedFromDraftMessage()
-    const auditRecordInput = buildAuditRecordInput(auditMessage, recordInput)
 
+    it('should handle get audit record failures', async () => {
+      mockCollection.find.mockImplementation(() => {
+        throw new Error('an error')
+      })
+      await expect(
+        getAuditRecords({
+          entityId: STUB_AUDIT_RECORD_ID
+        })
+      ).rejects.toThrow(new Error('an error'))
+    })
+  })
+
+  describe('createAuditRecord', () => {
     it('should create an audit record', async () => {
       await createAuditRecord(auditRecordInput, mockSession)
-      const [insertedAuditRecordInput] = mockCollection.insertOne.mock.calls[0]
+      const [insertedAuditRecordInput, session] =
+        mockCollection.insertOne.mock.calls[0]
       expect(insertedAuditRecordInput).toEqual(auditRecordInput)
+      expect(session).toEqual({ session: mockSession })
     })
 
     it('should handle failures', async () => {
