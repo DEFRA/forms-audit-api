0d1bd483af3d0337d9e5dd5a5472ea8f30c687aa
whitewater design <whitewaterdesign>
2025-08-08 21:03:30 +0100

    Revert "chore: test"
    This reverts commit 146e68d05c85edcf2d24bd33fd42af6dcb969948.

--- src/routes/form.js
+++ src/routes/form.js
@@ -1,17 +1,7 @@
-import {
-  AuditEventMessageCategory,
-  AuditEventMessageSchemaVersion,
-  AuditEventMessageSource,
-  AuditEventMessageType,
-  FormDefinitionRequestType,
-  idSchema
-} from '@defra/forms-model'
+import { idSchema } from '@defra/forms-model'
 import Joi from 'joi'
-import { ObjectId } from 'mongodb'
 
-import { STUB_MESSAGE_ID } from '~/src/api/forms/__stubs__/audit.js'
-
-// import { readAuditEvents } from '~/src/service/events.js'
+import { readAuditEvents } from '~/src/service/events.js'
 
 /**
  * @type {ServerRoute}
@@ -19,42 +9,13 @@ import { STUB_MESSAGE_ID } from '~/src/api/forms/__stubs__/audit.js'
 export default {
   method: 'GET',
   path: '/audit/forms/{id}',
-  handler(_request) {
-    // const { params } = request
-    // const { id } = params
+  handler(request) {
+    const { params } = request
+    const { id } = params
 
-    // return readAuditEvents({
-    //   entityId: id
-    // })
-    return [
-      {
-        _id: new ObjectId('68948579d5659369f1e634c6'),
-        id: '68948579d5659369f1e634c6',
-        messageId: STUB_MESSAGE_ID,
-        category: AuditEventMessageCategory.FORM,
-        type: AuditEventMessageType.FORM_UPDATED,
-        schemaVersion: AuditEventMessageSchemaVersion.V1,
-        source: AuditEventMessageSource.FORMS_MANAGER,
-        entityId: '68836f68210543a49431e4b2',
-        createdAt: new Date('2025-08-07T10:52:22.236Z'),
-        createdBy: {
-          id: '396e84b4-1cbd-40d0-af83-857be2aaefa7',
-          displayName: 'David Stone'
-        },
-        data: {
-          formId: '688131eeff67f889d52c66cc',
-          slug: 'my-form',
-          requestType: FormDefinitionRequestType.REPLACE_DRAFT,
-          s3Meta: {
-            fileId: 'jIEvk8KhqJfvbqE_uXYZtQ',
-            filename: '688131eeff67f889d52c66cc.json',
-            s3Key: 'audit-definitions/688131eeff67f889d52c66cc.json'
-          }
-        },
-        messageCreatedAt: new Date('2025-08-07T10:52:22.246Z'),
-        recordCreatedAt: new Date('2025-08-07T10:52:41.153Z')
-      }
-    ]
+    return readAuditEvents({
+      entityId: id
+    })
   },
   options: {
     auth: false,
--- src/routes/form.test.js
+++ src/routes/form.test.js
@@ -3,6 +3,7 @@ import { createServer } from '~/src/api/server.js'
 import { readAuditEvents } from '~/src/service/events.js'
 
 jest.mock('~/src/service/events.js')
+jest.mock('~/src/helpers/logging/logger.js')
 
 describe('Forms audit route', () => {
   /** @type {Server} */
