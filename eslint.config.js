dab641638454edeb6f2e6a2576059efba8e92b6c
Alex Luckett <alex@nodepoint.co.uk>
2026-06-05 10:38:26 +0100

    fix: revert eslint to v9 and upgrade eslint-plugin-jsdoc to v63
    neostandard declares peer eslint@"^9.0.0" and does not support v10,
    causing npm ci to fail with a peer conflict. Downgrade eslint to v9.39.4
    (latest v9) and fold in the jsdoc v63 upgrade (which supports both v9
    and v10) to resolve the CI failures on both PRs.

--- eslint.config.js
+++ eslint.config.js
@@ -118,6 +118,7 @@ export default [
       ],
 
       // JSDoc @param types are mandatory for JavaScript
+      'jsdoc/reject-any-type': 'off',
       'jsdoc/require-param-description': 'off',
       'jsdoc/require-param-type': 'error',
       'jsdoc/require-param': 'off',
@@ -149,6 +150,7 @@ export default [
       ...jsdocPlugin.configs['flat/recommended-typescript-flavor'].rules,
 
       // Preserve base rule overrides
+      'jsdoc/reject-any-type': 'off',
       'jsdoc/require-param-description': 'off',
       'jsdoc/require-param': 'off',
       'jsdoc/require-returns-description': 'off',
